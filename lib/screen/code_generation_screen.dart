d7af10d33d113a2b5920a8f6beb49fe025bcfd89
hexaspace <hexa6do@naver.com>
2023-07-06 13:50:06 +0900

    Study: consumer 위젯 사용

--- lib/screen/code_generation_screen.dart
+++ lib/screen/code_generation_screen.dart
@@ -57,6 +57,37 @@ class CodeGenerationScreen extends ConsumerWidget {
               ), // 로딩중 함수
             ),
             Text(state4.toString()),
+            // state5가 변경되었을때 consumer 위젯 안에서만 부분적으로 랜더링한다.
+            Consumer(
+              builder: (context, ref, child) {
+                final state5 = ref.watch(gStateNotifierProvider);
+                return Row(
+                  children: [
+                    Text('state5 : $state5'),
+                    if (child != null) child,
+                  ],
+                );
+              },
+              child: Text("hello"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref
+                    .read(
+                        gStateNotifierProvider.notifier) // 값을 바꾸고싶을때 .notifier
+                    .increment(); // 버튼 누를때 실행되는거 read
+              },
+              child: Text('up'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref
+                    .read(
+                        gStateNotifierProvider.notifier) // 값을 바꾸고싶을때 .notifier
+                    .decrement(); //불러와서 저장
+              },
+              child: Text('down'),
+            ),
             // invalidate 유효하지 않게 하다
             // state를 더이상 유효하지 않게해서 초기상태로 되돌림
             ElevatedButton(
